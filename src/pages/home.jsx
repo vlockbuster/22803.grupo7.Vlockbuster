2e55a12e9436bee64375c3f4f14d4c8df8647a2a
Jonathan Augusto Marcori <jonyls623@gmail.com>
2022-12-25 18:30:17 -0300

    otro

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { UserContext } from "../context/UserProvider";
-
+/* para usar la variable de estado hay que importar las 2 de arriba e 
+instanciarla como en la linea 8 y luego usarla */
 
  const Home = () => {
 
