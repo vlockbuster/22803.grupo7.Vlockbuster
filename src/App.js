b1c6888b09ab635942312fcaad1563daa4a3ff3f
figueduff <figueduff@hotmail.com>
2022-12-27 00:57:54 -0300

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Peliculas from './pages/peliculas';
 import Categorias from './pages/categorias';
 import Series from './pages/series';
 import NotFound from './pages/notFound';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {BrowserRouter, Route ,Routes} from 'react-router-dom';
 import Login from "./componentes/auth/login";
 import CrearUser from "./componentes/auth/crearUser";
 import User from "./componentes/auth/user";
@@ -20,16 +20,16 @@ const App = () => {
     <div className="App">
 
       <BrowserRouter>
-        <header className="App-header">
-          <NavBar />
-        </header>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/peliculas' element={<Peliculas />} />
-          <Route path='/categorias' element={<Categorias />} />
-          <Route path='/series' element={<Series />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
+      <header className="App-header">
+        <NavBar/>
+      </header>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/peliculas' element={<Peliculas/>}/>
+        <Route path='/categorias' element={<Categorias/>}/>
+        <Route path='/series' element={<Series/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
       </BrowserRouter>
       {/* <header className="App-header">
         <NavBar/>
