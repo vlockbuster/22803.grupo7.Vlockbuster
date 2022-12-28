982c88029fe9642b6607e373276e319d6df21da8
iara olivera <dev.iaraolivera@gmail.com>
2022-12-28 12:40:07 -0300

    modificacion del home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
+import Discovery from "../componentes/Discovery";
 import SearchResults from "../componentes/SearchResults";
 
 export default function home() {
   return (
     <>
       <SearchResults />
+      <Discovery />
     </>
   );
 }
