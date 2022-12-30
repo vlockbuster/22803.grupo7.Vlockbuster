98496a87d5df490aacb8cbf27f7949feea251ac5
figueduff <figueduff@hotmail.com>
2022-12-30 01:29:27 -0300

    update

--- src/componentes/Carrusel.js
+++ src/componentes/Carrusel.js
@@ -1,11 +1,14 @@
-
+import React from 'react'
 							
-const Carrusel = (props) => {
+function Carrusel({lista})  {
+
+	console.log("llego a carrusel jony", lista)
   
-	const arraymapeado = props.lista.results.map((item) => ({ "titulo": item.title, "url": item.poster_path, "id": item.id }));
+	const arraymapeado = lista.map((item) => ({ "titulo": item.title, "url": item.poster_path, "id": item.id }));
 	const corte1 = arraymapeado.splice(6)		
 	const corte2 = corte1.splice(6)		
-	const corte3 = corte2.splice(6)				
+	const corte3 = corte2.splice(6)		
+
 	return (
     <>
     
--- src/componentes/peliculas/masVistas.js
+++ src/componentes/peliculas/masVistas.js
@@ -1,19 +1,23 @@
 import React from "react";
-import { useState, useEffect,useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import BtAgregar from './btAgregarLista'
 import { Link } from 'react-router-dom'
 import BtVer from './btVer'
+import BtEliminar from './btEliminarDeLista'
 import { ListaContext } from "./contextLista";
+
+import Carrusel from "../Carrusel";
 
 function MasVistas() {
   const [pelis, setPelis] = useState([]);
   const [pagina, setPagina] = useState(1);
   const key = "e4e0f9c7c990f3921d36b5095affbe99"
   // const key = process.env.REACT_APP_KEY_TMDB
+
+  // estas 2 lineas son para mas abajo comprobar si existe la peli en la lista
   const [lista] = useContext(ListaContext)
+  let existeId = lista.map((item) => (item.id));
 
-
-console.log(lista)
   useEffect(() => {
     // fetch de api mas vistas
     const datos = async () => {
@@ -34,6 +38,7 @@ console.log(lista)
 
   return (
     <>
+     <Carrusel lista={pelis} />
       <div className="bg-dark p-1">
         <br />
         <h3 className="text-center text-light">Mas Populares</h3>
@@ -41,14 +46,15 @@ console.log(lista)
           {pelis.map((item, index) => (
             <div key={index} className="card bg-secondary p-1">
               <Link to={`/detalle/${item.id}`}>
-              <img
-                className="card-img-top p-1"
-                src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
-                data-dato={item.id}
-                alt={item.original_title}
-              />
+                <img
+                  className="card-img-top p-1"
+                  src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                  data-dato={item.id}
+                  alt={item.original_title}
+                />
               </Link>
-              <BtAgregar id={item.id} poster_path={item.poster_path} />
+              {/* aca esta la logica de botones si esta logueado ve uno u otro boton */}
+              {existeId.includes(item.id) ? <BtEliminar id={item.id} /> : <BtAgregar id={item.id} poster_path={item.poster_path} />}
               <BtVer id={item.id} />
             </div>
           ))}
