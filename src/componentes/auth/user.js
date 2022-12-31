f193bb0937d8ec93970a37aa8a4b0877daa649bf
figueduff <figueduff@hotmail.com>
2022-12-31 00:27:20 -0300

    update user

--- src/componentes/auth/user.js
+++ src/componentes/auth/user.js
@@ -38,7 +38,7 @@ function User() {
 
   return (
     <div>
-      {authUser ? (
+      {auth.currentUser ? (
         <>
           <p className="m-2 p-1">{`logueado ${auth.currentUser.email}`}</p>
           <button className="btn btn-secondary btn-badge m-2 p-1" onClick={desloguear}>Log Out</button>
